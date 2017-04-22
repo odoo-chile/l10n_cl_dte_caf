81b924cacd015153c658fafb8d556fe8eba463eb
dansanti <dansanti@gmail.com>
2017-04-22 02:59:14 -0300

    [IMP]Mejoramiento General Caf

--- models/caf.py
+++ models/caf.py
@@ -18,46 +18,74 @@ except ImportError:
 
 
 class caf(models.Model):
+
+    @api.depends('caf_file')
+    def _compute_data(self):
+        for caf in self:
+            if caf:
+                caf.load_caf()
+
     _name = 'dte.caf'
 
-    name = fields.Char('File Name', readonly=True, compute='_get_filename')
+    name = fields.Char('File Name',
+       readonly=True,
+       compute='_get_filename')
 
     filename = fields.Char('File Name')
 
     caf_file = fields.Binary(
-        string='CAF XML File', filters='*.xml', required=True,
-        store=True, help='Upload the CAF XML File in this holder')
+        string='CAF XML File',
+        filters='*.xml',
+        required=True,
+        help='Upload the CAF XML File in this holder')
 
     _sql_constraints=[(
         'filename_unique','unique(filename)','Error! Filename Already Exist!')]
 
-    issued_date = fields.Date('Issued Date')
+    issued_date = fields.Date('Issued Date',
+        compute='_compute_data',
+        store=True,)
 
-    sii_document_class = fields.Integer('SII Document Class')
+    sii_document_class = fields.Integer('SII Document Class',
+        compute='_compute_data',
+        store=True, )
 
     start_nm = fields.Integer(
-        string='Start Number', help='CAF Starts from this number')
+        string='Start Number',
+        help='CAF Starts from this number',
+        compute='_compute_data',
+        store=True, )
 
     final_nm = fields.Integer(
-        string='End Number', help='CAF Ends to this number')
+        string='End Number',
+        help='CAF Ends to this number',
+        compute='_compute_data',
+        store=True, )
 
     status = fields.Selection([
         ('draft', 'Draft'),
         ('in_use', 'In Use'),
         ('spent', 'Spent'),
         ('cancelled', 'Cancelled')], string='Status',
-        default='draft', help='''Draft: means it has not been used yet. You must put in in used
+        default='draft',
+        help='''Draft: means it has not been used yet. You must put in in used
 in order to make it available for use. Spent: means that the number interval
-has been exhausted. Cancelled means it has been deprecated by hand.''')
+has been exhausted. Cancelled means it has been deprecated by hand.''', )
 
-    rut_n = fields.Char(string='RUT')
+    rut_n = fields.Char(string='RUT',
+        compute='_compute_data',
+        store=True, )
 
     company_id = fields.Many2one(
-        'res.company', 'Company', required=False,
+        'res.company',
+        string='Company',
+        required=False,
         default=lambda self: self.env.user.company_id)
 
     sequence_id = fields.Many2one(
-        'ir.sequence', 'Sequence', required=False)
+        'ir.sequence',
+        'Sequence',
+        required=True)
 
     use_level = fields.Float(string="Use Level", compute='_use_level')
 
@@ -65,19 +93,13 @@ has been exhausted. Cancelled means it has been deprecated by hand.''')
     def load_caf(self, flags=False):
         if not self.caf_file:
             return
-        result = xmltodict.parse(
-            base64.b64decode(self.caf_file).replace(
-                '<?xml version="1.0"?>','',1))['AUTORIZACION']['CAF']['DA']
-
+        result = self.decode_caf()['AUTORIZACION']['CAF']['DA']
         self.start_nm = result['RNG']['D']
         self.final_nm = result['RNG']['H']
         self.sii_document_class = result['TD']
         self.issued_date = result['FA']
         self.rut_n = 'CL' + result['RE'].replace('-','')
-        if not self.sequence_id:
-            raise UserError(_(
-                'You should select a DTE sequence before enabling this CAF record'))
-        elif self.rut_n != self.company_id.vat.replace('L0','L'):
+        if self.rut_n != self.company_id.vat.replace('L0','L'):
             raise UserError(_(
                 'Company vat %s should be the same that assigned company\'s vat: %s!') % (self.rut_n, self.company_id.vat))
         elif self.sii_document_class != self.sequence_id.sii_document_class:
@@ -85,25 +107,17 @@ has been exhausted. Cancelled means it has been deprecated by hand.''')
                 '''SII Document Type for this CAF is %s and selected sequence associated document class is %s. This values should be equal for DTE Invoicing to work properly!''') % (self.sii_document_class, self.sequence_id.sii_document_class))
         if flags:
             return True
+        self.status = 'in_use'
 
-    @api.depends('start_nm', 'final_nm', 'sequence_id', 'status')
     def _use_level(self):
         for r in self:
             if r.status not in ['draft','cancelled']:
+                folio = r.sequence_id.number_next_actual
+                _logger.info(int(folio))
                 try:
-                    r.use_level = 100 * (float(r.sequence_id.number_next_actual - 1) / float(r.final_nm - r.start_nm + 1))
+                    r.use_level = 100.0 * ((int(folio) - r.start_nm) / float(r.final_nm - r.start_nm + 1))
                 except ZeroDivisionError:
                     r.use_level = 0
-                print r.use_level, r.sequence_id.number_next_actual, r.final_nm, r.start_nm
-                if r.sequence_id.number_next_actual > r.final_nm and r.status == 'in_use':
-                    #r.status = 'spent'
-                    self.env.cr.execute("""UPDATE dte_caf SET status = 'spent' WHERE filename = '%s'""" % r.filename)
-                    print 'spent'
-                elif r.sequence_id.number_next_actual <= r.final_nm and r.status == 'spent':
-                    #r.status = 'in_use'
-                    self.env.cr.execute("""UPDATE dte_caf SET status = 'in_use' WHERE filename = '%s'""" % r.filename)
-                    print 'in_use'
-
             else:
                 r.use_level = 0
 
