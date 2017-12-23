bc4858d9ecc46bf2ec91752b85bae720183c8ed4
Daniel Santibáñez Polanco <dansanti@gmail.com>
2017-12-23 19:46:28 -0300

    [REM] Código no Necesario
    - Los caf no se cancelan, folios o rangos particulares de un caf si
    - El caf debe ser siempre enabled, para que en el caso de cambio de caf esté disponible ( o por alguna razón un retimbraje)

--- __openerp__.py
+++ __openerp__.py
@@ -19,7 +19,7 @@
         'security/ir.model.access.csv',
     ],
     'update_xml': [],
-    'version': '9.0.6.8',
+    'version': '9.0.6.9',
     'website': 'http://blancomartin.cl',
     'auto-install': False,
     'active': False
